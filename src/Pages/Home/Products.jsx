76a5900019b2c0277efbba086be17149c191c5fb
Shahed Ahmed <shahedahmedutsho@gmail.com>
2024-08-17 17:26:02 +0600

    pagination bar

--- src/Pages/Home/Products.jsx
+++ src/Pages/Home/Products.jsx
@@ -6,7 +6,15 @@ import { PuffLoader } from 'react-spinners';
 import { Star } from 'lucide-react';
 import { Fade } from 'react-awesome-reveal';  // Import Fade from react-awesome-reveal
 import defaultImg from '../../assets/VC203.webp';
-
+import {
+    Pagination,
+    PaginationContent,
+    PaginationEllipsis,
+    PaginationItem,
+    PaginationLink,
+    PaginationNext,
+    PaginationPrevious,
+  } from "@/components/ui/pagination"
 const Products = () => {
     const [guitars,setGuitars] = useState([])
     const { products, isLoading, isError, refetch , sortFn} = useFetch();
@@ -99,6 +107,33 @@ const Products = () => {
                             </Card>
                         </Fade>
                     ))}
+                </div>
+                <div className='fixed  right-10 flex items-center !justify-start  bg-slate-900/0 bottom-4  h-10 '>
+                <Pagination className=" max-w-96 mx-0 bg-slate-300  dark:bg-slate-700 rounded-md   justify-start">
+      <PaginationContent>
+        <PaginationItem>
+          <PaginationPrevious href="#" />
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink href="/">1</PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink href="#" isActive>
+            2
+          </PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink href="#">3</PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationEllipsis />
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationNext href="#" />
+        </PaginationItem>
+      </PaginationContent>
+      
+    </Pagination>
                 </div>
             </div>
         );
